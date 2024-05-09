b512393d5974a2e8471a23defa11624fc41f4a52
shaarawy29 <ashaarawy19@gmail.com>
2024-05-09 20:40:58 +0300

    there are two line glitches because of the score_display component

--- project_1.srcs/sources_1/new/game2p.vhd
+++ project_1.srcs/sources_1/new/game2p.vhd
@@ -574,7 +574,9 @@ begin
             else
                 curr_pixel <= frame_pixel;
             end if;
-        elsif (unsigned(pos_x(9 downto 3)) >= 10 and unsigned(pos_x(9 downto 3)) <= 24 and unsigned(pos_y) <= 31) then
+        elsif ((unsigned(pos_x) >= 80) and (unsigned(pos_x) <= 200) and (unsigned(pos_y) >= 0) and (unsigned(pos_y) <= 15)) then
+            curr_pixel <= score_pixel;
+        elsif ((unsigned(pos_x) >= 120) and (unsigned(pos_x) <= 200) and (unsigned(pos_y) >= 16) and (unsigned(pos_y) <= 31)) then
             curr_pixel <= score_pixel;
         -- outside the display area
         else
